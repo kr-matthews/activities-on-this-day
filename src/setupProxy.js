9fafb2b3cc911fdd1a2b830d2016d841e57649e4
Kevin Matthews <kvnrmatthews@gmail.com>
2022-10-13 22:14:59 -0700

    Fix proxy
    can't use ES Modules

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-export default function (app) {
+module.exports = function (app) {
   app.use(
     "/.netlify/functions/",
     createProxyMiddleware({
@@ -10,4 +10,4 @@ export default function (app) {
       },
     })
   );
-}
+};
