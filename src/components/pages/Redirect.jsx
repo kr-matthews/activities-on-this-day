bfcc92a8606e1f8430501f9ff7e5a50b0a75b8c9
Kevin Matthews <kvnrmatthews@gmail.com>
2022-11-20 09:22:46 -0800

    Handle all possible errors on redirect page
    No permission provided; wrong permission provided; error using
    permission to get token.

--- src/components/pages/Redirect.jsx
+++ src/components/pages/Redirect.jsx
@@ -6,45 +6,63 @@ import Error from "../Error";
 
 import { useFetchData } from "../../hooks/useFetchData";
 
-// !! ERROR - handle 'error=access_denied' in url
-// !! ERROR - can check whether they gave correct permissions?
-
 export default function Redirect({ setRefreshToken }) {
-  const params = useSearchParams();
-  const code = params[0].get("code");
-  const { data, isLoading, error, fetch } = useFetchData();
   const navigate = useNavigate();
+  const params = useSearchParams();
+
+  const code = params[0].get("code");
+
+  const scope = params[0].get("scope");
+  // good: read,activity:read_all --- bad: read
+  const hasCorrectScope = scope && scope.includes("read_all");
+
+  const error = params[0].get("error");
+  // bad: access_denied
+  const hasPermissionDenied = error && error.includes("denied");
+
+  const { data, isLoading, error: fetchError, fetch } = useFetchData();
 
   useEffect(() => {
-    if (code) {
+    if (code && hasCorrectScope) {
       console.info("Fetching refresh token.");
       fetch([`/.netlify/functions/get-refresh-token?code=${code}`]);
     }
-  }, [code, fetch]);
+  }, [code, hasCorrectScope, fetch]);
 
   useEffect(() => {
-    if (data) {
+    if (data && hasCorrectScope) {
       console.info("Received refresh token; saving it.");
       setRefreshToken(data);
       navigate("/", { replace: true });
     }
-  }, [data, navigate, setRefreshToken]);
+  }, [data, hasCorrectScope, navigate, setRefreshToken]);
 
-  // !! ERROR - add no code error case
-  // !! ERROR - descriptive error message/UI, based on 2 possible errors
-  // !! UI - tidy up redirect UI
+  // !!! UI - tidy up redirect UI - check strava codes
   return (
     <>
+      {hasPermissionDenied && (
+        <Error task="provide permission" message="permission denied" />
+      )}
+
+      {!hasPermissionDenied && !hasCorrectScope && (
+        <Error
+          task="provide permission"
+          message="insufficient permissions provided"
+        />
+      )}
+
       {isLoading && <Loading task="fetch persistent token" />}
-      {error && (
-        <>
-          <Error
-            task="fetch persistent token"
-            statusCode={error.statusCode}
-            message={error.message}
-          />
-          <button onClick={() => navigate("/authenticate")}>Try again</button>
-        </>
+
+      {fetchError && (
+        <Error
+          task="fetch persistent token"
+          statusCode={fetchError.statusCode}
+          message={fetchError.message}
+        />
+      )}
+
+      {(hasPermissionDenied || !hasCorrectScope || fetchError) && (
+        <button onClick={() => navigate("/authenticate")}>Try again</button>
       )}
     </>
   );
